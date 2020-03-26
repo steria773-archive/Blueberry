0991ffd7aeec159ab57275e182e32d03361362da
Rabia Alhaffar <steria773@gmail.com>
2020-03-26 14:57:54 -0400

    Added Tutorials

---
+++ tutorials/blueberry_tutorial_1.py
@@ -0,0 +1,9 @@
+import blueberry
+from blueberry import *
+
+#Creates A Game Window 
+#With Title And Size Of 600x600 
+init(600,600,"Our Game Window") 
+
+#Wait For User Input(In Python Console)
+input()
